e0c33c232b89eef162a45054d5ebb16feaac3959
Alini <alini.canedo@gmail.com>
2023-07-15 10:31:31 -0300

    layout login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
