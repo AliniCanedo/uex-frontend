ba1ecd450eb46f953cd5f94249e03a72e5982d6e
Alini <alini.canedo@gmail.com>
2023-07-16 00:21:14 -0300

    rm code

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
